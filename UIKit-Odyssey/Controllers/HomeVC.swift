12ac04168f23a490393c7b1a82cb47f831b50299
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-08-05 23:53:29 +0530

    added UI

---
+++ UIKit-Odyssey/Controllers/HomeVC.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  UIKit-Odyssey
+//
+//  Created by Jaimin Raval on 05/08/24.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var rSlider: UISlider!
+    
+    @IBOutlet weak var gSlider: UISlider!
+    
+    @IBOutlet weak var bSlider: UISlider!
+    
+    @IBOutlet weak var opacitySlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func rSliderChanged(_ sender: Any) {
+    }
+    
+    @IBAction func gSliderChanged(_ sender: Any) {
+    }
+    
+    @IBAction func bSliderChanged(_ sender: Any) {
+    }
+    
+    @IBAction func opacitySliderChanged(_ sender: Any) {
+    }
+    
+
+
+}
+
