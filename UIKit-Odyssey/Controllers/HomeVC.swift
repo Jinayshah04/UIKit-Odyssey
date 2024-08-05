4600e8e68ccdd744a886c45ece8add8caf9ef77c
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-08-06 00:24:32 +0530

    added code for color change
    using slider user can change color of UIView.

--- UIKit-Odyssey/Controllers/HomeVC.swift
+++ UIKit-Odyssey/Controllers/HomeVC.swift
@@ -19,21 +19,64 @@ class HomeVC: UIViewController {
     
     @IBOutlet weak var opacitySlider: UISlider!
     
+    private var rValue: CGFloat!
+    private var gValue: CGFloat!
+    private var bValue: CGFloat!
+    private var opacityValue: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        setColor(r: 0, g: 0, b: 0, o: 0)
+    }
+    
+    func setColor(r: CGFloat, g: CGFloat, b: CGFloat, o: CGFloat) {
+        
+        colorView.backgroundColor =  UIColor(red: r/255, green: g/255, blue: b/255, alpha: o)
+    }
+    
+    
+    @IBAction func rSliderChanged(_ sender: Any) {
+        
+        rValue =  CGFloat(rSlider.value)
+        gValue =  CGFloat(gSlider.value)
+        bValue =  CGFloat(bSlider.value)
+        opacityValue =  CGFloat(opacitySlider.value)
+        
+        setColor(r: rValue, g: gValue, b: bValue, o: opacityValue)
         
     }
     
-    @IBAction func rSliderChanged(_ sender: Any) {
-    }
-    
     @IBAction func gSliderChanged(_ sender: Any) {
+        
+        rValue =  CGFloat(rSlider.value)
+        gValue =  CGFloat(gSlider.value)
+        bValue =  CGFloat(bSlider.value)
+        opacityValue =  CGFloat(opacitySlider.value)
+        
+        setColor(r: rValue, g: gValue, b: bValue, o: opacityValue)
+
     }
     
     @IBAction func bSliderChanged(_ sender: Any) {
+        
+        rValue =  CGFloat(rSlider.value)
+        gValue =  CGFloat(gSlider.value)
+        bValue =  CGFloat(bSlider.value)
+        opacityValue =  CGFloat(opacitySlider.value)
+        
+        setColor(r: rValue, g: gValue, b: bValue, o: opacityValue)
+
     }
     
     @IBAction func opacitySliderChanged(_ sender: Any) {
+        
+        rValue =  CGFloat(rSlider.value)
+        gValue =  CGFloat(gSlider.value)
+        bValue =  CGFloat(bSlider.value)
+        opacityValue =  CGFloat(opacitySlider.value)
+        
+        setColor(r: rValue, g: gValue, b: bValue, o: opacityValue)
+
     }
     
 
